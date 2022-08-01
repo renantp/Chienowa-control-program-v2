0eea0fc751379c021f7fd45ec53c3c7d63ef67e7
renantp <phuannguyenhoang@gmail.com>
2022-08-01 16:10:11 +0700

    Add e module files

---
+++ src/chienowa/module/e_module.c
@@ -0,0 +1,130 @@
+/*
+ * e_module.c
+ *
+ *  Created on: Jul 29, 2022
+ *      Author: Renan
+ */
+
+#include "e_module.h"
+
+union Error_status g_error_status;
+
+int e1051_water_leak_error(void){
+	return 0;
+}
+
+int e1035_salt_tank_drain_valve_direction_error(void){
+	return 0;
+}
+
+int e1034_saltwater_drain_direction_error(void){
+	return 0;
+}
+
+int e1033_total_electrolysis_time_error(void){
+	return 0;
+}
+
+int e1032_total_power_on_time(void){
+	return 0;
+}
+
+int e10311_filter_replace(void){
+	return 0;
+}
+
+int e1031_continue_filter_alarm(void){
+	return 0;
+}
+
+int e1030_filter_replace_alarm(void){
+	return 0;
+}
+
+int e1029_alkali_tank_empty(void){
+	return 0;
+}
+
+int e1028_acid_tank_error(void){
+	return 0;
+}
+
+int e1026_acid_and_alkali_tank_over_flow_error(void){
+	return 0;
+}
+
+int e1025_alkali_tank_level_sensor_error(void){
+	return 0;
+}
+
+int e1024_acid_tank_level_sensor_error(void){
+	return 0;
+}
+
+int e1023_salt_tank_level_sensor_error(void){
+	return 0;
+}
+
+int e1022_salt_tank_cap_error(void){
+	return 0;
+}
+
+int e1021_empty_salt_tank(void){
+	return 0;
+}
+
+int e1020_salt_tank_error(void){
+	return 0;
+}
+
+int e1009_cvcc_error(void){
+	return 0;
+}
+
+int e1008_sv1_error(void){
+	return 0;
+}
+
+int e1007_current_20_error(void){
+	return 0;
+}
+
+int e1006_current_10_error(void){
+	return 0;
+}
+
+int e1005_low_voltage_error(void){
+	return 0;
+}
+
+int e1004_over_voltage_3(void){
+	return 0;
+}
+
+int e1003_over_voltage_2(void){
+	return 0;
+}
+
+int e1002_over_voltage_1(void){
+	return 0;
+}
+
+int e1001_low_flow_rate_error(void){
+	return 0;
+}
+
+int e1000_flow_sensor_over_range(void){
+	return 0;
+}
+
+int e2_naturalizing_process(void){
+	return 0;
+}
+
+int e11_do_refresh(void){
+	return 0;
+}
+
+int e1_faucet_refresh_process(void){
+	return 0;
+}
---
+++ src/chienowa/module/e_module.h
@@ -0,0 +1,24 @@
+/*
+ * e_module.h
+ *
+ *  Created on: Jul 29, 2022
+ *      Author: Hung
+ */
+
+#ifndef CHIENOWA_MODULE_E_MODULE_H_
+#define CHIENOWA_MODULE_E_MODULE_H_
+
+#include "../../r_cg_macrodriver.h"
+
+extern union Error_status {
+	struct{
+		int over_voltage_1: 1;
+		int over_voltage_2: 1;
+		int over_voltage_3: 1;
+
+		uint32_t rsvd: 29;
+	};
+	uint32_t data;
+}g_error_status;
+
+#endif /* CHIENOWA_MODULE_E_MODULE_H_ */
