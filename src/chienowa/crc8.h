8f263c023fabfea8327246db0983ae786ad35bd3
renantp <phuannguyenhoang@gmail.com>
2022-08-12 10:51:15 +0700

    Add crc files

---
+++ src/chienowa/crc8.h
@@ -0,0 +1,15 @@
+/*
+ * crc8.h
+ *
+ *  Created on: Aug 10, 2022
+ *      Author: Renan
+ */
+
+#ifndef CHIENOWA_CRC8_H_
+#define CHIENOWA_CRC8_H_
+
+#include "../r_cg_macrodriver.h"
+
+uint8_t crc8(uint8_t *p_data, uint32_t len);
+
+#endif /* CHIENOWA_CRC8_H_ */
