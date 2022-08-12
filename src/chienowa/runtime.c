f78e1a59929576e8bc73c9637e971d2e0e12ac7b
renantp <phuannguyenhoang@gmail.com>
2022-08-12 10:54:52 +0700

    Add runtime()

---
+++ src/chienowa/runtime.c
@@ -0,0 +1,12 @@
+/*
+ * runtime.c
+ *
+ *  Created on: Aug 11, 2022
+ *      Author: Renan
+ */
+#include "eeprom.h"
+
+void runtime(void){
+	eeprom_runtime();
+}
+
---
+++ src/chienowa/runtime.h
@@ -0,0 +1,15 @@
+/*
+ * runtime.h
+ *
+ *  Created on: Aug 11, 2022
+ *      Author: Renan
+ */
+
+#ifndef CHIENOWA_RUNTIME_H_
+#define CHIENOWA_RUNTIME_H_
+
+#include "../r_cg_macrodriver.h"
+
+void runtime(void);
+
+#endif /* CHIENOWA_RUNTIME_H_ */
